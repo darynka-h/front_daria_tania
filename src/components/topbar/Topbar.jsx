38d1693626b1f5179a8088f51059131054424bca
Tetiana <149360911+pythotonix@users.noreply.github.com>
2024-03-13 14:40:53 +0200

    Update Topbar.jsx

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,31 +1,39 @@
 import './topbar.css';
-// import {Home, Search, Person, CalendarMonth} from '@mui/icons-material'
+import {Button, Modal} from "react-bootstrap";
+import Addpost from '../addpost/addpost';
+import {Link} from 'react-router-dom';
+import {AddBox, Search, Face2, CalendarMonth} from '@mui/icons-material';
+import React from 'react';
+
 export default function Topbar() {
+    const [modalShow, setModalShow] = React.useState(false);
+
     return (
         <div className='topbarContainer'>
+            <Addpost show={modalShow} onHide={() => setModalShow(false)}/>
             <div className="topbarLeft">
-                <span className="logo">CUCU</span>
+                <Link to="/"> <span className="logo">CUCU</span> </Link>
             </div>
             <div className="topbarCenter">
                 <div className="searchbar">
-                    {/* <Search className='searchIcon'/> */}
+                    <Search className='searchIcon'/>
                     <input placeholder="Search for friend, post or video" 
                            className="searchInput" color="#eff0f6"/>
                 </div>
             </div>
             <div className="topbarRight">
                 <div className="topbarIcons">
-                    {/* <div className="topbarIconItem">
-                        <Home/>
+                    <div className="topbarIconItem">
+                        <AddBox onClick={() => setModalShow(true)}/>
                     </div>
                     <div className="topbarIconItem">
-                        <Person/>
+                        <Face2/>
                     </div>
                     <div className="topbarIconItem">
                         <CalendarMonth/>
-                    </div> */}
+                    </div>
                 </div>
-                <img src="/assets/userphoto1.jpg" alt="" className="topbarImg"/>  
+                <Link to="/personal" ><img src="/assets/userphoto1.jpg" alt="" className="topbarImg"/> </Link> 
             </div>
         </div>
     );
